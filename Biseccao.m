98fbcc24e615a9e83a6df44190c4b7928aebb303
Felipe Roque de Albuquerque Neto <roquefelipe1972@hotmail.com>
2019-08-29 21:41:35 -0300

    Update Bisecção
    organização dos valores na hora de imprimir a tabela de iterações

--- Biseccao.m
+++ Biseccao.m
@@ -3,11 +3,8 @@
 %Autor: Felipe Roque de Albuquerque Neto
 %Curso: Bacharelado em Engenharia Elétrica
 
-clear
-clc
-
 %Recebe a Função desejada
-f = @(x) ;%% Digite sua função nesta área após @(x)
+f = @(x) x^2+log(x);%% Digite sua função nesta área entre parenteses e aspas simples
 
 %Recebe os Intervalos desejados
 xa = input('Insira o valor do Intervalo Xa: '); %Recebe o valor do intervalo A
@@ -16,23 +13,28 @@ xb = input('Insira o valor do Intervalo Xb: '); %Recebe o valor do intervalo B
 %Recebe a Tolerancia desejada
 tolerancia_desejada = input('Insira a tolerancia: '); %Recebe o valor do erro desejado
 
-%Processamento dos Dados
-if((f(xa)*f(xb))>0)
+%Processamento de Dados
+disp('I      Xa          Xb        X_media      f(Xa)       f(Xb)');
+if f(xa)*f(xb)>0
     fprintf('Não existe raiz o intervalo inserido!');
     return
 else
     while(abs(xb-xa)>tolerancia_desejada)
+    i = i + 1;%Contator de iterações
     x_media = (xa+xb)/2;
+    fprintf('%d \t%f \t%f \t%f \t%f \t%f \n',i,xa,xb,x_media,f(xa),f(xb));
     if((f(xa)*f(x_media))<0)
         xb = x_media;
     else
         xa = x_media;
     end
-     if abs(f(xa))<tolerancia_desejada
+    
+    if abs(f(xa))<tolerancia_desejada
         break
     end
     end
 end
+
 
 %Exibindo Resultado da Operação
 fprintf('A Raiz é: %f',x_media);
