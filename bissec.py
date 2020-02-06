b81ebecb7a38d1c630db1cde926f5dda07f34616
Felipe Roque de Albuquerque Neto <roquefelipe1972@hotmail.com>
2020-02-06 13:20:08 -0300

    Add files via upload

---
+++ bissec.py
@@ -0,0 +1,38 @@
+# Método da Bisecção - Cálculo Numérico
+# Desenvolvido - Felipe Roque de Albuquerque Neto
+# Instituto Federal de Pernambuco - Campus Recife
+# Curso de Bacharelado em Engenharia Mecânica
+
+#Importamos a biblioteca de matemática
+import math 
+
+# Definição dos intervalos [a,b]
+a = float(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+print("\n")
+
+# Definição da tolerancia
+e = float(input("Digite a tolerancia desejada: "))
+print("\n")
+# Construção da Funçãa para entrada das equações
+def f(x):
+    return x**2 +math.log(x) # Equação escolhida
+print("a \tb   \t\tx_media   \t\t\tf(a) \t\t\t\tf(b)                   \t\t\tf(x_media)")
+print("\n")
+# Teoremo de Bolzano
+if (f(a)*f(b)) < 0:
+    while (math.fabs(b-a)/2 > e):
+        x_media = (a+b)/2
+        print("{} \t{}\t\t{}    \t\t{} \t\t{}                           \t\t{}".format(a,b,x_media,f(a),f(b),f(x_media)))
+        if f(x_media) == 0:
+            print(" A Raiz é: {}".format(x_media))
+        else:
+            if f(a)*f(x_media) < 0:
+                b = x_media
+            else: 
+                a = x_media
+    print("\n")
+    print("Valor da raiz é: {} ".format(x_media))
+    print("\n")
+else:
+    print("Não há raiz neste intervalo!")
