b9daaca7b9152d1cfe71f824f8127c27916905c9
feliperoque <roquefelipe1972@hotmail.com>
2020-05-11 15:00:16 -0300

    Finalizado!

--- ponto_fixo.py
+++ ponto_fixo.py
@@ -9,15 +9,24 @@
 # Bibliotecas
 import math
 
-# Função do cálculo do Ponto_Fixo
-def ponto_fixo(x0, erro, n_de_iteracoes):
-    x1 = f(x0)
-    if (abs(x1-x0)<erro):
-        return x1
+# Função que receberá nossa Equação:
+def f(x):
+    return (math.sin(x) + math.cos(x))/2 # Inserir a equação aqui
 
+
+def ponto_fixo(f,x,tol):
+    erro = 1
     iteracao = 0
 
-    while ((n_de_iteracoes > iteracao) and (abs(x1-x0) > = erro)):
-        iteracao+=1
+    while erro > tol:
+        x_novo = f(x)
+        erro = abs(x_novo-x)
+        x = x_novo
+        iteracao +=1
+        print(f'x{iteracao} = {x: 0.6f}') # Printa as iterações feitas
+    print('\n')
 
-
+    # Printa o Resultado final
+    print(f'Raiz Encontrada: {x: 0.6f}\nQuantidade Iteracões Feitas: {iteracao}') 
+    print('\n')
+ponto_fixo(f, 10, 0.001)
